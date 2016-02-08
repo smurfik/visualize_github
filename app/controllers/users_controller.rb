7b9d029a9a34bb8e337d9490323b2e88df07b656
smurfik <smurfik@mlpinit.com>
2016-02-08 14:08:10 -0800

    add config for omniauth and user's controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def create
+    oath_attrs = request.env['omniauth.auth']
+    @user = User.find_or_initialize_by(uid: oath_attrs.uid)
+    @user.username   = oath_attrs.info.nickname
+    @user.email      = oath_attrs.info.email
+    @user.avatar_url = oath_attrs.info.image
+    @user.token      = oath_attrs.credentials.token
+    @user.save
+    session[:user_id] = @user.id
+    redirect_to root_path
+  end
+
+  def sign_out
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+
+end
